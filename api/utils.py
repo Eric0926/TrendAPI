fbcad42f4cf53f5ac7492ff4e6dd9f323e8ec00a
Eric0926 <38851446+Eric0926@users.noreply.github.com>
2020-08-24 00:31:01 -0400

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -1,3 +1,4 @@
+import json
 from google.cloud import spanner
 
 client = spanner.Client("yiqing-twitter-candidates")
@@ -30,4 +31,17 @@ if __name__ == "__main__":
     tops = sorted(results_sorted[:10], key=lambda x: x[0])
     tops_id = ",".join(str(x[0]) for x in tops)
     tops_info = database.run_in_transaction(fetch_candidates, tops_id)
-    print(tops_info)
+    trends = []
+    for i in range(10):
+        x = {}
+        x["name"] = tops_info[i][1]
+        x["party"] = tops_indo[i][2]
+        x["state"] = tops_info[i][3] if tops_info[i][3] is not None else "none"
+        x["twitterID"] = tops_info[i][4] if tops_info[i][4] is not None else "none"
+        x["reply"] = tops[i][2]
+        x["toxic"] = tops[i][3]
+        x["retweet"] = tops[i][4]
+        trends.append(x)
+    trends_json = json.dumps(trends)
+    for x in trends_json:
+        print(x)
