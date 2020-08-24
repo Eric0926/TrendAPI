0089f7ee56576e88d0d9ccb1709740e099146fff
Eric0926 <38851446+Eric0926@users.noreply.github.com>
2020-08-24 00:08:10 -0400

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -14,11 +14,11 @@ def fetch_stats(transaction):
     return list(result)
 
 
-def fetch_candidates(transaction):
+def fetch_candidates(transaction, tops_id):
     query = """
             SELECT * FROM candidate_table 
             WHERE candidate_id IN ({})
-            """.format("138203134, 15764644, 818948638890217473, 29501253, 13218102")
+            """.format(tops_id)
     result = transaction.execute_sql(query)
     return list(result)
 
@@ -29,5 +29,5 @@ if __name__ == "__main__":
         results, key=lambda result: (-result[2], -result[3]))
     tops = sorted(results_sorted[:10], key=lambda x: x[0])
     tops_id = ",".join(str(x[0]) for x in tops)
-    tops_info = database.run_in_transaction(fetch_candidates)
+    tops_info = database.run_in_transaction(fetch_candidates, tops_id)
     print(tops_info)
