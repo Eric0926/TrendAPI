8bd8e1d093f7db3a330320b0c3e97e1da377fd42
Eric0926 <38851446+Eric0926@users.noreply.github.com>
2020-12-14 00:33:36 -0500

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -150,10 +150,10 @@ def last_n_days(candidate_id, n):
             for tweet_id in tweet_ids:
                 if len(data["examples"]) >= 10:
                     break
-                key = Key('tweets', tweet_id, project=PROJECT_ID_DATASTORE)
+                key = Key('tweets', int(tweet_id),
+                          project=PROJECT_ID_DATASTORE)
                 query = datastore_client.query(kind='tweets')
                 query.add_filter('__key__', '=', key)
-                print(tweet_id)
                 tweet = list(query.fetch(1))[0]
                 data["examples"].append(tweet['text'])
                 data["example_urls"].append(tweet['urls'][0] if len(
